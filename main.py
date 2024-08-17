7623da20bd97fb116d74cc1ce196369c543db947
YaelBodor <bodor.yael@gmail.com>
2024-08-17 17:57:40 +0300

    Feat: add heatmap when classify

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 import io
 import uvicorn
-import easyocr
+# import easyocr
+from torchvision import transforms
 from fastapi import HTTPException, FastAPI, UploadFile, File
 from fastapi.middleware.cors import CORSMiddleware
 from PIL import Image
+from fastapi.responses import StreamingResponse
 from starlette.responses import JSONResponse
+import base64
 
 from models.ingredients_analysis import get_ingredients_analysis
 from models.skin_type_by_face_image.skin_analysis import get_skin_analysis
 
 app = FastAPI()
 
-reader = easyocr.Reader(['en'])  # You can specify multiple languages if needed
+# reader = easyocr.Reader(['en'])  # You can specify multiple languages if needed
 
 # Enable CORS
 app.add_middleware(
@@ -30,42 +33,54 @@ def read_root():
 
 @app.post("/skin-analysis/")
 async def skin_analysis(file: UploadFile = File(...)):
-    # Read the image file
-    image: Image = Image.open(io.BytesIO(await file.read()))
+    image: Image = Image.open(io.BytesIO(await file.read())).convert('RGB')
 
-    class_label = get_skin_analysis(image)
+    transform = transforms.Compose([
+        transforms.Resize((128, 128)),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])  # Normalize to [-1, 1]
+    ])
 
-    return class_label
+    img = transform(image).unsqueeze(0).to('cpu')
+
+    img_io, predicted_class = get_skin_analysis(img)
+
+    img_base64 = base64.b64encode(img_io.getvalue()).decode('utf-8')
+
+    return JSONResponse(content={
+        "predicted_class": predicted_class,
+        "heatmap": img_base64
+    })
 
 
-@app.post("/ingredients-list")
-async def extract_text_from_image(file: UploadFile = File(...)):
-    try:
-        # Read the file contents and convert the file contents to an image
-        image = Image.open(io.BytesIO(await file.read()))
-
-        image = image.convert("RGB")
-        # Convert the image back to bytes
-        img_byte_arr = io.BytesIO()
-        image.save(img_byte_arr, format='JPEG')  # Save the image as JPEG or appropriate format
-        img_byte_arr = img_byte_arr.getvalue()
-
-        # Use easyocr to read text from the image bytes
-        result = reader.readtext(img_byte_arr, detail=0, paragraph=True)
-
-        # Combine the text results
-        text = " ".join(result)
-        print(text)
-
-        predicted_skin_types = get_ingredients_analysis(text)
-        print(predicted_skin_types)
-
-        return JSONResponse(content=predicted_skin_types)
-    except Exception as e:
-        print(e)
-        raise HTTPException(status_code=500, detail=str(e))
+#
+# @app.post("/ingredients-list")
+# async def extract_text_from_image(file: UploadFile = File(...)):
+#     try:
+#         # Read the file contents and convert the file contents to an image
+#         image = Image.open(io.BytesIO(await file.read()))
+#
+#         image = image.convert("RGB")
+#         # Convert the image back to bytes
+#         img_byte_arr = io.BytesIO()
+#         image.save(img_byte_arr, format='JPEG')  # Save the image as JPEG or appropriate format
+#         img_byte_arr = img_byte_arr.getvalue()
+#
+#         # Use easyocr to read text from the image bytes
+#         result = reader.readtext(img_byte_arr, detail=0, paragraph=True)
+#
+#         # Combine the text results
+#         text = " ".join(result)
+#         print(text)
+#
+#         predicted_skin_types = get_ingredients_analysis(text)
+#         print(predicted_skin_types)
+#
+#         return JSONResponse(content=predicted_skin_types)
+#     except Exception as e:
+#         print(e)
+#         raise HTTPException(status_code=500, detail=str(e))
 
 
 if __name__ == "__main__":
     uvicorn.run(app, host="localhost", port=8001)
-
