6406fb57be12a197f315be8a6cc40ab97b2b4d87
oriyafe1 <ori987yafe@gmail.com>
2024-08-20 16:18:08 +0300

    remove some prints and try except clauses that wer unnecessary

--- main.py
+++ main.py
@@ -29,6 +29,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+
 class User(BaseModel):
     username: str
     password: str
@@ -51,81 +52,68 @@ async def skin_analysis(file: UploadFile = File(...)):
 
 @app.post("/ingredients-list")
 async def extract_text_from_image(file: UploadFile = File(...)):
-    try:
-        # Read the file contents and convert the file contents to an image
-        image = Image.open(io.BytesIO(await file.read()))
+    # Read the file contents and convert the file contents to an image
+    image = Image.open(io.BytesIO(await file.read()))
 
-        image = image.convert("RGB")
-        # Convert the image back to bytes
-        img_byte_arr = io.BytesIO()
-        image.save(img_byte_arr, format='JPEG')  # Save the image as JPEG or appropriate format
-        img_byte_arr = img_byte_arr.getvalue()
+    image = image.convert("RGB")
+    # Convert the image back to bytes
+    img_byte_arr = io.BytesIO()
+    image.save(img_byte_arr, format='JPEG')  # Save the image as JPEG or appropriate format
+    img_byte_arr = img_byte_arr.getvalue()
 
-        # Use easyocr to read text from the image bytes
-        result = reader.readtext(img_byte_arr, detail=0, paragraph=True)
+    # Use easyocr to read text from the image bytes
+    result = reader.readtext(img_byte_arr, detail=0, paragraph=True)
 
-        # Combine the text results
-        text = " ".join(result)
-        print(text)
+    # Combine the text results
+    text = " ".join(result)
+    print(text)
 
-        predicted_skin_types = get_ingredients_analysis(text)
-        print(predicted_skin_types)
+    predicted_skin_types = get_ingredients_analysis(text)
+    print(predicted_skin_types)
 
-        return JSONResponse(content=predicted_skin_types)
-    except Exception as e:
-        print(e)
-        raise HTTPException(status_code=500, detail=str(e))
+    return JSONResponse(content=predicted_skin_types)
+
 
 # Register route to save user data in a list format
 @app.post("/register/")
 async def register(user: User):
-    print("a")
-    try:
-        # Load the existing users from the file, if it exists
-        if USERS_FILE.exists():
-            print("b")
-            with USERS_FILE.open("r") as f:
-                users = json.load(f)
-        else:
-            users = []
-        print(users)
-        # Check if the username already exists in the list
-        for existing_user in users:
-            if existing_user['username'] == user.username:
-                raise HTTPException(status_code=400, detail="Username already exists")
-        print("dd")
-        # Add the new user to the users list
-        users.append({"username": user.username, "password": user.password})
+    # Load the existing users from the file, if it exists
+    if USERS_FILE.exists():
+        with USERS_FILE.open("r") as f:
+            users = json.load(f)
+    else:
+        users = []
+    # Check if the username already exists in the list
+    for existing_user in users:
+        if existing_user['username'] == user.username:
+            raise HTTPException(status_code=400, detail="Username already exists")
+    print("dd")
+    # Add the new user to the users list
+    users.append({"username": user.username, "password": user.password})
 
-        # Save the updated users list back to the JSON file
-        with USERS_FILE.open("w") as f:
-            json.dump(users, f, indent=4)
+    # Save the updated users list back to the JSON file
+    with USERS_FILE.open("w") as f:
+        json.dump(users, f, indent=4)
 
-        return {"message": "User registered successfully"}
+    return {"message": "User registered successfully"}
 
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
 
 @app.post("/login/")
 async def login(user: User):
-    try:
-        # Load the existing users from the file
-        if USERS_FILE.exists():
-            with USERS_FILE.open("r") as f:
-                users = json.load(f)
-        else:
-            raise HTTPException(status_code=400, detail="No users found")
+    # Load the existing users from the file
+    if USERS_FILE.exists():
+        with USERS_FILE.open("r") as f:
+            users = json.load(f)
+    else:
+        raise HTTPException(status_code=400, detail="No users found")
 
-        # Check if the user exists in the users list
-        for existing_user in users:
-            if existing_user['username'] == user.username and existing_user['password'] == user.password:
-                return {"message": "Login successful"}
+    # Check if the user exists in the users list
+    for existing_user in users:
+        if existing_user['username'] == user.username and existing_user['password'] == user.password:
+            return {"message": "Login successful"}
 
-        raise HTTPException(status_code=400, detail="Invalid username or password")
+    raise HTTPException(status_code=400, detail="Invalid username or password")
 
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
 
 if __name__ == "__main__":
     uvicorn.run(app, host="localhost", port=8001)
-
